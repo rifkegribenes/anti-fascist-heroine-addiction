e615372862a72791839b79a8a37c1a63a83adec1
Sarah Schneider <rifkegribenes@gmail.com>
2017-07-15 23:27:54 -0700

    project boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => (
+    <main>
+      <div className="container">
+        <h1>dungeon crawler</h1>
+      </div>
+    </main>
+    );
+
+export default App;
