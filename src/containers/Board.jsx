ba493fa00e331d86245c9753fc67efb58de30bff
Sarah Schneider <rifkegribenes@gmail.com>
2017-07-22 15:21:50 -0700

    added handlers for animals & food

--- src/containers/Board.jsx
+++ src/containers/Board.jsx
@@ -10,10 +10,16 @@ class Board extends Component {
       entities: [[]],
       gameLevel: 0,
       heroPosition: [],
+      hero: {
+        hp: 100,
+        xp: 0,
+        attack: 10,
+      },
+      messages: [],
     };
 
     this.handleKeydown = this.handleKeydown.bind(this);
-    this.playerInput = this.playerInput.bind(this);
+    this.userInput = this.userInput.bind(this);
   }
   componentWillMount() {
 
@@ -38,35 +44,54 @@ class Board extends Component {
     switch (e.keyCode) {
       case 38:
       case 87:
-        this.playerInput([0, -1]);
+        this.userInput([0, -1]);
         break;
       case 39:
       case 68:
-        this.playerInput([1, 0]);
+        this.userInput([1, 0]);
         break;
       case 40:
       case 83:
-        this.playerInput([0, 1]);
+        this.userInput([0, 1]);
         break;
       case 37:
       case 65:
-        this.playerInput([-1, 0]);
+        this.userInput([-1, 0]);
         break;
       default:
 
     }
   }
 
-  playerInput(change) {
+  userInput(change) {
     const [x, y] = this.state.heroPosition;
     const [changeX, changeY] = change;
     const newPosition = [changeX + x, changeY + y];
     const newHero = this.state.entities[y][x];
-    // const destination = this.state.entities[y + changeY][x + changeX];
+    const destination = this.state.entities[y + changeY][x + changeX];
     // console.log('moving', newHero, destination);
-    this.changeEntity({ type: 'floor' }, [x, y]);
-    this.changeEntity(newHero, newPosition);
-    this.changeHeroPosition(newPosition);
+    if (destination.type !== 'wall' && destination.type !== 'monster' && destination.type !== 'boss') {
+      this.changeEntity({ type: 'floor' }, [x, y]);
+      this.changeEntity(newHero, newPosition);
+      this.changeHeroPosition(newPosition);
+    }
+    // handle collisions
+    switch (destination.type) {
+      case 'finalMonster':
+      case 'monster':
+        this.handleCombat(destination);
+        break;
+      case 'food':
+        this.modifyHP(destination);
+        break;
+      case 'animal':
+        this.addAnimal(destination);
+        break;
+      case 'staircase':
+        this.handleStaircase(destination);
+        break;
+      default:
+    }
   }
 
   changeEntity(entity, coords) {
@@ -103,6 +128,51 @@ class Board extends Component {
     });
   }
 
+  addAnimal(animal) {
+    const hero = Object.assign({}, this.state.hero);
+    const messages = [...this.state.messages];
+    hero.attack += animal.damage;
+    messages.push(`You added ${animal.name} to your team! ${animal.message}, and adds ${animal.damage} points of damage to your team attack.`);
+    this.setState({
+      hero,
+      messages,
+    }, () => {
+      // console.log(hero);
+    });
+  }
+
+  // addXP() {}
+
+  modifyHP(entity, delta) {
+    const hero = Object.assign({}, this.state.hero);
+    const messages = [...this.state.messages];
+    if (entity.type === 'food') {
+      const healthBoost = entity.healthBoost;
+      hero.hp += healthBoost;
+      messages.push(`You ate ${entity.name} and gained ${entity.healthBoost} health points!`);
+    } else {
+      hero.hp += delta;
+    }
+    this.setState({
+      hero,
+      messages,
+    }, () => {
+      // console.log(hero);
+    });
+  }
+
+  // restartGame() {}
+
+  // toggleTorch() {}
+
+  handleCombat(monster) {
+    console.log(this, `Your team is attacking ${monster.name}!`);
+  }
+
+  // handleCollision() {}
+
+  // handleStaircase() {}
+
   startGame() {
     const { newMap, heroPosition } = fillGrid(generateMap());
     this.setState({
@@ -114,8 +184,10 @@ class Board extends Component {
   }
 
   render() {
+// render messages to a ul, return only 3 most recent, style
     return (
       <div>
+        <div className="message">{this.state.messages}</div>
         {/* } <button onClick={(e) => { toggleTorch; }} /> */}
         <canvas
           id="board"
