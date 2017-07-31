e87cbd1c3dd2cf85103f3c0143e1f8e88196649d
Sarah Schneider <rifkegribenes@gmail.com>
2017-07-30 17:00:31 -0700

    coded food urls

--- src/utils/foodTypes.js
+++ src/utils/foodTypes.js
@@ -1,21 +1,21 @@
 // Hero health start: 100
 
 // Cherry 1 15
-// Strawberry 1 15
+// Raspberry 1 15
 // Apple 1 15
 // Kiwi 1 15
 // Total health possible: 60 (160 total)
 // total damage range (@2-3 hits each): 120-180
 
-// Watermelon 2 30
-// Grapes 2 30
+// Orange 2 30
+// Plum 2 30
 // Pear 2 30
-// Pineapple 2 30
+// Popsicle 2 30
 // Total health possible: 120 (280 total)
 // total damage range (@1-2 hits each): 120-180 (240-360 total)
 
 // Ice cream 3 50
-// Popsicle 3 50
+// Cupcake 3 50
 // Hamburger 3 50
 // Donut 3 80
 // Total health possible: 230 (510 total)
@@ -26,87 +26,96 @@ const foodTypes = [
   {
     name: 'a Cherry',
     title: 'Cherries',
-    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/cherries.gif',
-    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/cherries_200.gif',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/cherries_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/cherries_200.png',
     healthBoost: 15,
     level: 1,
   },
   {
-    name: 'a Strawberry',
+    name: 'a Raspberry',
     title: 'Strawberry',
-    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/strawberry.gif',
-    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/strawberry_200.gif',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/raspberry_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/raspberry_200.png',
     healthBoost: 15,
     level: 1,
   },
   {
     name: 'an Apple',
     title: 'Apple',
-    iconUrl: '',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/apple_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/apple_200.png',
     healthBoost: 15,
     level: 1,
   },
   {
     name: 'a Kiwi',
     title: 'Kiwi',
-    iconUrl: '',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/kiwi_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/kiwi_200.png',
     healthBoost: 15,
     level: 1,
   },
   {
-    name: 'a Watermelon',
-    title: 'Watermelon',
-    iconUrl: '',
+    name: 'an Orange',
+    title: 'Orange',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/orange_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/orange_200.png',
     healthBoost: 30,
     level: 2,
   },
   {
-    name: 'a bunch of Grapes',
-    title: 'Grapes',
-    iconUrl: '',
+    name: 'a Plum',
+    title: 'Plum',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/plum_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/plum_200.png',
     healthBoost: 30,
     level: 2,
   },
   {
     name: 'a Pear',
     title: 'Pear',
-    iconUrl: '',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/pear_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/pear_200.png',
     healthBoost: 30,
     level: 2,
   },
   {
-    name: 'a Pineapple',
-    title: 'Pineapple',
-    iconUrl: '',
+    name: 'a Popsicle',
+    title: 'Popsicle',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/popsicle_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/popsicle_200.png',
     healthBoost: 30,
     level: 2,
   },
   {
     name: 'an Ice Cream Cone',
     title: 'Ice Cream Cone',
-    message: '',
-    iconUrl: '',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/ice-cream_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/ice-cream_200.png',
     healthBoost: 50,
     level: 3,
   },
   {
-    name: 'a Popsicle',
-    title: 'Popsicle',
-    iconUrl: '',
+    name: 'a Cupcake',
+    title: 'Cupcake',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/cupcake_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/cupcake_200.png',
     healthBoost: 50,
     level: 3,
   },
   {
     name: 'a Hamburger',
     title: 'Hamburger',
-    iconUrl: '',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/hamburger_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/hamburger_200.png',
     healthBoost: 50,
     level: 3,
   },
   {
     name: 'a Donut',
     title: 'Donut',
-    iconUrl: '',
+    iconUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/donut_32.png',
+    cardUrl: 'https://raw.githubusercontent.com/rifkegribenes/dungeon-crawler/master/src/img/donut_200.png',
     healthBoost: 80,
     level: 3,
   },
