60f093f47413842c5b55dbb7491bf32f193f4665
Sarah Schneider <rifkegribenes@gmail.com>
2017-11-03 15:29:38 -0700

    superhero art source info

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -4,6 +4,23 @@ import { bindActionCreators } from 'redux';
 import { Link } from 'react-router-dom';
 
 import * as Actions from '../store/actions';
+import teamHeroes from '../utils/teamHeroes';
+
+const heroCredits = teamHeroes.map((hero) => {
+  return (
+    <div className="splash__item">
+      <div className="splash__avatar-wrap">
+        <img src={hero.cardUrl} alt={hero.name} className="card-pic card-pic--round" />
+      </div>
+      <div className="splash__text-wrap">
+        <span className="splash__bold">{hero.name}</span><br />
+          Character created by: {hero.createdBy || ''}<br />
+          Image source: <a className="splash__link" href={hero.srcUrl}>{hero.srcName}</a><br />
+          Artwork by: {hero.artBy || 'Unknown'}
+      </div>
+    </div>
+  );
+});
 
 const About = () => (
   <div className="splash">
@@ -12,9 +29,11 @@ const About = () => (
         <h2 className="splash__title">Credits</h2>
       </div>
       <div className="splash__instructions">
-        <h3 className="splash__subhead">How to Play</h3>
-        <h4 className="splash__bold">Moving your character</h4>
-          Blah blah blah.<br />
+        <h3 className="splash__subhead">Artwork</h3>
+        <h4 className="splash__bold">Superheroes:</h4>
+        <div className="splash__list">
+          {heroCredits}
+        </div>
         <div className="splash__btn-wrap">
           <Link className="big-msg__btn" to="/">How to play</Link>
           <Link className="big-msg__btn" to="/hero-picker">Start Game</Link>
